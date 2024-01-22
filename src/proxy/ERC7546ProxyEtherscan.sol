7179f27450cfb24b2e6219a99b97c022821bd01e
Kai Hiroi <kai.hiroi@pm.me>
2024-01-22 13:04:24 +0900

    Update: Etherscan-compatible Proxy

--- src/proxy/ERC7546ProxyEtherscan.sol
+++ src/proxy/ERC7546ProxyEtherscan.sol
@@ -1,30 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-/// @dev Library version has been tested with version 5.0.0.
-import {Proxy} from "openzeppelin-contracts/contracts/proxy/Proxy.sol";
 import {ERC1967Utils} from "openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Utils.sol";
+import {StorageSlot} from "openzeppelin-contracts/contracts/utils/StorageSlot.sol";
 
-import {IDictionary} from "../dictionary/IDictionary.sol";
+import {ERC7546Proxy} from "./ERC7546Proxy.sol";
+
 
 /**
- * @title ERC7546: Proxy Contract
+ * @title Etherscan-compatible ERC7546Proxy Contract
  */
-contract ERC7546ProxyEtherscan is Proxy {
-    /**
-     * @notice Specification 2.2.1
-     */
-    constructor(address dictionary, bytes memory _data) payable {
-        ERC1967Utils.upgradeBeaconToAndCall(dictionary, _data);
+contract ERC7546ProxyEtherscan is ERC7546Proxy {
+    constructor(address dictionary, bytes memory _data) payable ERC7546Proxy(dictionary, _data) {
+        _setBeacon(dictionary);
     }
 
-    /**
-     * @notice Specification 2.2.2
-     * @dev Return the implementation address corresponding to the function selector.
-     */
-    function _implementation() internal view override returns (address) {
-        return IDictionary(ERC1967Utils.getBeacon()).getImplementation(msg.sig);
+    function _setBeacon(address _newBeacon) internal {
+        StorageSlot.getAddressSlot(ERC1967Utils.BEACON_SLOT).value = _newBeacon;
+        emit ERC1967Utils.BeaconUpgraded(_newBeacon);
     }
-
-    receive() external payable {}
 }
