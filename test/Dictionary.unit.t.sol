5efee4806ca5ed4ae9688e23af4a055d2cbf98fe
Kai Hiroi <kai.hiroi@pm.me>
2024-03-29 10:41:04 +0900

    update naming & specs

--- test/Dictionary.unit.t.sol
+++ test/Dictionary.unit.t.sol
@@ -16,16 +16,16 @@ contract DictionaryUnitTest is Test, Dictionary {
 
     /**
      *  Dictionary
-     *    (1) MUST implement `getImplementation(bytes4 functionSelector)`
-     *          to return Function Implementation Contract address.
-     *    (2) SHOULD implement `setImplementation(bytes4 functionSelector, address implementation)`
-     *      (2-1) to update or add new function selectors and their corresponding Function Implementation Contract addresses to the mapping.
-     *      (2-2) and SHOULD be communicated through an event (or log).
-     *    (3) is RECOMMENDED to implement
-     *      (3-1) `supportsInterface(bytes4 interfaceID)` defined in ERC-165
-     *              to indicate which interfaces are supported by the contracts referenced in the mapping.
-     *      (3-2) `supportsInterfaces()`
-     *              to return a list of registered interfaceIDs
+     *    (1) getImplementation(bytes4 functionSelector)
+     *          MUST return Function Implementation Contract address.
+     *    (2) setImplementation(bytes4 functionSelector, address implementation)
+     *      (2-1) SHOULD add new function selectors and their corresponding Function Implementation Contract addresses to the mapping.
+     *      (2-1) SHOULD update
+     *      (2-2) SHOULD be communicated through an event (or log).
+     *    (3) supportsInterface(bytes4 interfaceID) defined in ERC-165
+     *          is RECOMMENDED to indicate which interfaces are supported by the contracts referenced in the mapping.
+     *    (4) supportsInterfaces()
+     *          is RECOMMENDED to return a list of registered interfaceIDs
      */
 
     /// @dev Verify (1)
@@ -47,22 +47,22 @@ contract DictionaryUnitTest is Test, Dictionary {
     }
 
 
-    function complete_setImplementation(bytes4 functionSelector, address implementation) internal {
+    function execute_setImplementation_WithContractAddr(bytes4 functionSelector, address implementation) internal {
         Helper.assertContract(implementation);
         dictionary.setImplementation(functionSelector, implementation);
-        assertEq(implementations[functionSelector], implementation);
     }
 
     /// @dev Verify (2-1)
     function test_setImplementation_Success_SetToMapping(bytes4 _fuzz_functionSelector, address _fuzz_implementation) public {
-        complete_setImplementation(_fuzz_functionSelector, _fuzz_implementation);
+        execute_setImplementation_WithContractAddr(_fuzz_functionSelector, _fuzz_implementation);
+        assertEq(implementations[_fuzz_functionSelector], _fuzz_implementation);
     }
 
     /// @dev Verify (2-2)
     function test_setImplementation_Success_EmitCorrectEvent(bytes4 _fuzz_functionSelector, address _fuzz_implementation) public {
         vm.expectEmit();
         emit Dictionary.ImplementationUpgraded(_fuzz_functionSelector, _fuzz_implementation);
-        complete_setImplementation(_fuzz_functionSelector, _fuzz_implementation);
+        execute_setImplementation_WithContractAddr(_fuzz_functionSelector, _fuzz_implementation);
     }
 
     function test_setImplementation_Revert_InvalidImplementation_WhenNonContract(bytes4 _fuzz_functionSelector, address _fuzz_implementation) public {
@@ -71,4 +71,50 @@ contract DictionaryUnitTest is Test, Dictionary {
         dictionary.setImplementation(_fuzz_functionSelector, _fuzz_implementation);
     }
 
+    /**
+        @notice Verify (3-1)
+     */
+    function test_Success_supportsInterface(bytes4 _fuzz_functionSelector, address _fuzz_implementation) public {
+        test_Success_setImplementation_getImplementation(_fuzz_functionSelector, _fuzz_implementation);
+        assertTrue(dictionary.supportsInterface(_fuzz_functionSelector));
+    }
+
+    /**
+        @notice Verify (3-2)
+     */
+    function test_Success_supportsInterfaces(bytes4[100] calldata _fuzz_functionSelector, address[100] calldata _fuzz_implementation) public {
+        for (uint i; i < _fuzz_functionSelector.length;) {
+            test_Success_setImplementation_getImplementation(_fuzz_functionSelector[i], _fuzz_implementation[i]);
+            unchecked {
+                ++i;
+            }
+        }
+
+        bytes4[] memory interfaces = Dictionary(dictionary).supportsInterfaces();
+        if (interfaces.length == _fuzz_functionSelector.length) {
+            for (uint i; i < _fuzz_functionSelector.length;) {
+                assertEq(interfaces[i], _fuzz_functionSelector[i]);
+                unchecked {
+                    ++i;
+                }
+            }
+        } else {
+            for (uint i; i < interfaces.length;) {
+                bool isMatch;
+                for (uint j; j < _fuzz_functionSelector.length;) {
+                    if (interfaces[i] == _fuzz_functionSelector[j]) {
+                        isMatch = true;
+                        break;
+                    }
+                    unchecked {
+                        ++j;
+                    }
+                }
+                assertTrue(isMatch);
+                unchecked {
+                    ++i;
+                }
+            }
+        }
+    }
 }
