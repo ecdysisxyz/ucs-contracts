5fc65ca2e8e2f8a697e42ad19b9deed0b12953a8
Kai Hiroi <kai.hiroi@pm.me>
2024-05-20 14:48:43 +0900

    Add BeaconDictionary Test

--- test/dictionary/BeaconDictionaryTest.sol
+++ test/dictionary/BeaconDictionaryTest.sol
@@ -2,34 +2,35 @@
 pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
-import {Helper} from "./utils/Helper.sol";
-import {Dummy} from "./utils/Dummy.sol";
+import {Helper} from "test/utils/Helper.sol";
+import {Dummy} from "test/utils/Dummy.sol";
 
-import {BeaconDictionary} from "../src/dictionary/extensions/BeaconDictionary.sol";
+import {BeaconDictionary} from "src/dictionary/BeaconDictionary.sol";
 
 /**
  *  @title A test to verify that the Dictionary Contract meets the specifications of the ERC-7546 standard.
  *
  *  Dictionary Specs
  *    (1) getImplementation(bytes4 functionSelector)
- *          MUST return Function Implementation Contract address.
  */
 contract BeaconDictionaryTest is Test, BeaconDictionary {
     BeaconDictionary internal dictionary = BeaconDictionary(address(this));
-    constructor() BeaconDictionary(address(new Dummy()), address(this)) {}
+    address initialImpl = Dummy.contractAddress();
+    constructor() BeaconDictionary(initialImpl, address(this)) {}
 
 
     /**--------------------------
         (1) getImplementation
     ----------------------------*/
     //  (1) Positive
-    function test_getImplementation_Success_ReturnCorrectAddress(bytes4 _fuzz_functionSelector, address _fuzz_implementation) public {
-        vm.assume(_fuzz_implementation != address(0));
+    function test_getImplementation_Success_ReturnCorrectAddress(bytes4 _fuzz_selector, address _fuzz_implementation) public {
+        assertEq(dictionary.getImplementation(_fuzz_selector), initialImpl);
+
         Helper.assertContract(_fuzz_implementation);
         dictionary.upgradeTo(_fuzz_implementation);
 
-        assertEq(dictionary.getImplementation(_fuzz_functionSelector), _fuzz_implementation);
-        assertEq(dictionary.implementation(), _fuzz_implementation);
+        assertEq(_fuzz_implementation, dictionary.getImplementation(_fuzz_selector));
+        assertEq(_fuzz_implementation, dictionary.implementation());
     }
 
 }
