867043d48081ba440d865c1bc70c0fbf438939dd
Kai Hiroi <kai.hiroi@pm.me>
2024-05-20 14:46:22 +0900

    Add ImmutableDictionary

---
+++ src/dictionary/ImmutableDictionary.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {DictionaryBase} from "./base/DictionaryBase.sol";
+import {console2} from "forge-std/console2.sol";
+struct Function {
+    bytes4 selector;
+    address implementation;
+}
+
+/**
+    @title ERC7546: Immutable Dictionary Contract
+ */
+contract ImmutableDictionary is DictionaryBase {
+    error SelectorAlreadyExists(bytes4 selector);
+
+    constructor(Function[] memory _functions, address _facade) {
+        for (uint i; i < _functions.length; ++i) {
+            _setImplementation(_functions[i].selector, _functions[i].implementation);
+        }
+        _upgradeFacade(_facade);
+    }
+
+    function __existsSameSelector(bytes4 _selector) internal override returns(bool) {
+        for (uint i; i < functionSelectorList.length; ++i) {
+            if (functionSelectorList[i] == _selector) revert SelectorAlreadyExists(_selector);
+        }
+        return false;
+    }
+
+}
